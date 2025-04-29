fdaf68ff36f35aeb1ce4e25e6ada4ca78009182b
paul-le <55872209+paul-le@users.noreply.github.com>
2025-04-29 11:18:10 +0200

    Correction d'animation
    Animation Parallax dans titre Studio Koukaki

--- wp-content/themes/foce-child02/scripts/script.js
+++ wp-content/themes/foce-child02/scripts/script.js
@@ -30,14 +30,18 @@
     // $("#characters").fadeOut()
     $("#oscarsSection").fadeOut()
     let nuage=false;
+    const wrapper = document.getElementById("studio");
+    const paralx0 = document.getElementById("paralx0");
+    const paralx1 = document.getElementById("paralx1");
+
 
     $(window).scroll(function(){
 
         if ($(document).scrollTop() >= 50) {
             $(".story").fadeIn(3000);
-            console.log("story");
+            // console.log("story");
         }
-
+        
         if ($(document).scrollTop() >= 1300) {
             $("#studio").fadeIn(3000);
         }
@@ -46,12 +50,17 @@
             $("#oscarsSection").fadeIn(3000);
         }
 
+        if ($(document).scrollTop() >= 2280) {;
+          $("paralx0").fadeIn(1000);
+      }
+
         if (nuage == false && $(document).scrollTop() > 700) {
             $(".bigCloudAnim").animate({right: "+=300"} , 3000);
             $(".littleCloudAnim").animate({right: "+=800"} , 3000);
             nuage = true;
         }
         console.log($(document).scrollTop())
+        
     })
 
     // $(".bigCloudAnim").animate({Left: "50px"} ,2000);
@@ -80,3 +89,60 @@ var swiper = new Swiper(".mySwiper", {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
    });
+
+
+// const wrapper = document.getElementById("studio");
+// const paralx0 = document.getElementById("paralx0");
+// const paralx1 = document.getElementById("paralx1");
+
+// wrapper.onscroll = function () {
+//   let y = wrapper.scrollTop;
+//   console.log("hallo");
+//   paralx0.style.top = 35 - y * 1.25 + "px";
+//   paralx1.style.top = 90 - y * 0.75 + "px";
+// };
+
+
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+  var tags = $(".fadein")
+  var tags2 = $(".fadein2")
+
+  for (var i = 0; i < tags.length; i++ ){
+    var tag = tags[i]
+
+    if ($(tag).offset().top < pageBottom) {
+      $(tag).addClass("visible")
+    } else {
+      $(tag).removeClass("visible")
+    }
+  }
+
+  // for (var i2 = 0; i2 < tags2.length; i2++ ){
+  //   var tag2 = tags2[i]
+
+  //   if ($(tag).offset().top < pageBottom) {
+  //     $(tag2).addClass("visible2")
+  //   } else {
+  //     $(tag2).removeClass("visible2")
+  //   }
+  // }
+})
+
+// $(document).on("scroll", function () {
+//   var pageTop = $(document).scrollTop()
+//   var pageBottom = pageTop + $(window).height()
+//   var tags2 = $(".fadein2")
+
+//   for (var i2 = 0; i2 < tags2.length; i2++) {
+//     var tag2 = tags2[i2]
+
+//     if ($(tag2).offset().top < pageBottom) {
+//       $(tag2).addClass("visible2")
+//     } else {
+//       $(tag2).removeClass("visible2")
+//     }
+//   }
+// })
+
